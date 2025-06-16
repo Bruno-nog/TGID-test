aa58a2a9eb54138f33dc92d46caf5fa4a269e8ed
Bruno Nogueira de Queiroz <bqueiroz.nog@gmail.com>
2025-06-16 19:05:02 -0300

    function addToCart completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,20 +2,38 @@ const btnCart = document.querySelector('.addcart');
 cartSideBar = document.querySelector('.cart-sidebar');
 const btnAddCart = document.querySelector('.btn-add-cart');
 const products = document.querySelector('.products');
+let cart = [];
 
 
 products.addEventListener('click', function(event){
-    if (event.target.matches('.btn-add-cart'))
-    {
-        let closest = btnAddCart.closest('.products');
-        console.log(closest);
-        
-    }
+    if (!event.target.matches('.btn-add-cart')) return;
+    const valueItem = event.target.closest('.item');
+    let {id, name, price, image} = valueItem.dataset;
+    const product = {id, name, price, image};
+    addToCart(product);   
     
 })
+
+function addToCart(product)
+{
+    const existing = cart.find(item => item.id === product.id);
+    if (existing)
+    {
+        existing.quantity += 1;
+        console.log(existing);
+    }
+    else
+    {
+        product.quantity = 1;
+        cart.push(product);
+    }
+    
+}
 
 btnCart.addEventListener('click', function(){
     console.log(btnCart);
     cartSideBar.classList.toggle('show');
     
 })
+
+
